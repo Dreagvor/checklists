730f47a4900c9af55872d9cba065fb7dbe409fe2
Yauheni <kamesh2403@gmail.com>
2017-11-24 21:31:44 +0300

    add Delete

--- App/CheckLists/CheckListController.swift
+++ App/CheckLists/CheckListController.swift
@@ -44,6 +44,15 @@ class CheckListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
     func configureCheckmark(for cell: UITableViewCell,
                             with item: CheckListItem) {
         if item.checked {
