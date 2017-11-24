662181f9d9bfcd1a2a66aa1d5ce375081e44c4aa
Yauheni <kamesh2403@gmail.com>
2017-11-24 22:25:31 +0300

    additem

--- App/CheckLists/AddItemViewController.swift
+++ App/CheckLists/AddItemViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func done() {
+        
         dismiss(animated: true, completion: nil)
     }
     override func tableView(_ tableView: UITableView,willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        if newText.length > 0 {
+            doneBarButton.isEnabled = true
+        } else {
+            doneBarButton.isEnabled = false
+        }
+        return true
     }
 }
